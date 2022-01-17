4fb2bea9043c818e89493e4afee3193f539657de
Pycomet <alfredemmanuelinyang@gmail.com>
2022-01-17 12:42:03 +0100

    deploy

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import webbrowser
 from config import *
 from models import Token
 from typing import KeysView
@@ -11,38 +12,62 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-# # LOCAL SERVER WITH GECKODRIVER
-# options = Options()
-# options.headless = True
+def load_driver():
+    # # LOCAL SERVER WITH GECKODRIVER
+    if DEBUG == True:
+        options = Options()
+        options.headless = True
 
-# driver = webdriver.Firefox()
-# wait = WebDriverWait(driver, 10000) # Huge amount of delay response
+        driver = webdriver.Firefox()
+
+
+    else:
+        options = webdriver.FirefoxOptions()
+        # enable trace level for debugging 
+        options.log.level = "trace"
+
+        options.add_argument("-remote-debugging-port=9224")
+        options.add_argument("-headless")
+        options.add_argument("-disable-gpu")
+        options.add_argument("-no-sandbox")
+
+        binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
+
+        driver = webdriver.Firefox(
+            firefox_binary=binary,
+            executable_path=os.environ.get('GECKODRIVER_PATH'),
+            options=options
+        )
+
+    return driver
 
 ##############
 
 
 # # SERVER PROCESSING WITH CHROME DRIVER (HEROKU)
-options = webdriver.ChromeOptions()
+# options = webdriver.ChromeOptions()
 
-options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-sh-usage")
+# options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# options.add_argument("--headless")
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-sh-usage")
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
-wait = WebDriverWait(driver, 10000) # Huge amount of delay response
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
+# wait = WebDriverWait(driver, 10000) # Huge amount of delay response
 
 def pull_tx_info(tx:str, token:Token):
     "Fetch Data From EtherScan Website Page"
+    driver = load_driver()
+    wait = WebDriverWait(driver, 100) # Huge amount of delay response
 
     print("Fetching Transaction Data ...")
 
     driver.get(f"https://etherscan.io/tx/{tx}")
 
-    # import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
 
-    # wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div[3]/div[1]/div[2]/div[1]/div/div[5]/div[2]/ul/li/div/a[1]"))) # For page to load complete
-    time.sleep(20)
+    wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div[3]/div[1]/div[2]/div[1]/div/div[5]/div[2]/ul/li/div/a[1]"))) # For page to load complete
+    # time.sleep(20)
     entry1 = driver.find_element_by_xpath('/html/body/div[1]/main/div[3]/div[1]/div[2]/div[1]/div/div[5]/div[2]/ul/li/div/a[1]').text
     entry2 = driver.find_element_by_xpath('/html/body/div[1]/main/div[3]/div[1]/div[2]/div[1]/div/div[5]/div[2]/ul/li/div/a[2]').text
 
